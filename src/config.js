c3ac17bd4f42d90484be039009655e18271feba6
matteo-schwanGIT <Matteo.schwan@gmail.com>
2026-03-23 15:07:15 +0100

    Add config.js file with initial configuration.

---
+++ src/config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const config = {
+  TOKEN: 'your_token_here',
+  PREFIX: '!',
+  OWNER_ID: 'your_owner_id_here',
+  SERVER_ID: 'your_server_id_here'
+};
+
+module.exports = config;
